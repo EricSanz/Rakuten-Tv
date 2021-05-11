b6d70177be96b7227537ce2daccad53915f18fea
Eric Sanz <eric_sanz_rodriguez@hotmail.com>
2021-05-11 21:01:43 +0200

    updated navbar desktop css

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const subscriptionsMenu = document.getElementById('navmenu__subscriptions-id');
 const subscriptionsMenuOptions = document.getElementById('subscriptions__options-id');
 const subscriptionsTitle = document.getElementById('subscriptions__text');
+const cinemaMenu = document.getElementById('navmenu__cinema-id');
+const cinemaMenuOptions = document.getElementById('cinema__options-id');
+const cinemaTitle = document.getElementById('cinema__text');
 
 // Navbar Dropdowns Functionalities//
 
@@ -22,3 +25,4 @@ subscriptionsMenuOptions.addEventListener('mouseout', () => {
     subscriptionsMenuOptions.style.display = 'none';
     subscriptionsTitle.style.color = '#fff';
 }, false);
+
