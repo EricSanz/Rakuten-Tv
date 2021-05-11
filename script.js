d05286f22cf45e4294519a1f1b31adec3fbee498
Eric Sanz <eric_sanz_rodriguez@hotmail.com>
2021-05-11 20:09:44 +0200

    deleted commented lines and updated navbar dropdowns functionalities

--- script.js
+++ script.js
@@ -2,10 +2,17 @@ const subscriptionsMenu = document.getElementById('navmenu__subscriptions-id');
 const subscriptionsMenuOptions = document.getElementById('subscriptions__options-id');
 const subscriptionsTitle = document.getElementById('subscriptions__text');
 
+// Navbar Dropdowns Functionalities//
+
 subscriptionsMenu.addEventListener('mouseover', () => {
     subscriptionsMenuOptions.style.display = 'block';
     subscriptionsTitle.style.color = '#ffe300';
 }, false);
+
+subscriptionsMenu.addEventListener('mouseout', () => {
+    subscriptionsMenuOptions.style.display = 'none';
+    subscriptionsTitle.style.color = '#fff';
+})
 
 subscriptionsMenuOptions.addEventListener('mouseover', () => {
     subscriptionsTitle.style.color = '#ffe300';
