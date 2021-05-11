a17fee4780f3294a837476a86e24872b85e084e6
Eric Sanz <eric_sanz_rodriguez@hotmail.com>
2021-05-11 19:59:36 +0200

    menu dropdown functionality done

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const subscriptionsMenu = document.getElementById('navmenu__subscriptions-id');
 const subscriptionsMenuOptions = document.getElementById('subscriptions__options-id');
+const subscriptionsTitle = document.getElementById('subscriptions__text');
 
 subscriptionsMenu.addEventListener('mouseover', () => {
     subscriptionsMenuOptions.style.display = 'block';
+    subscriptionsTitle.style.color = '#ffe300';
+}, false);
+
+subscriptionsMenuOptions.addEventListener('mouseover', () => {
+    subscriptionsTitle.style.color = '#ffe300';
 }, false);
 
 subscriptionsMenuOptions.addEventListener('mouseout', () => {
     subscriptionsMenuOptions.style.display = 'none';
+    subscriptionsTitle.style.color = '#fff';
 }, false);
